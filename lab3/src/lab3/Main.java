94f1392a93faa7ae89043f5ffc32dcff03f0562b
DanilLyalin <danil.lyalin@gmail.com>
2020-04-15 22:45:31 +0300

    Correct multithreading implemeneted

--- lab3/src/lab3/Main.java
+++ lab3/src/lab3/Main.java
@@ -3,24 +3,26 @@ package lab3;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Robots started their work.");
         try
         {
+
             Broker broker = new Broker();
 
             ExecutorService threadPool = Executors.newFixedThreadPool(4);
-
             threadPool.execute(new Consumer(Constants.subjects[0], broker));
             threadPool.execute(new Consumer(Constants.subjects[1], broker));
             threadPool.execute(new Consumer(Constants.subjects[2], broker));
             Future producerStatus = threadPool.submit(new Producer(broker));
 
-            // this will wait for the producer to finish its execution.
             producerStatus.get();
             threadPool.shutdown();
+
         }
         catch (Exception e)
         {
